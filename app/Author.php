4b854cb233b13830814d3ba7e519d9aeb7da0195
Kirill Matasov <kirill.matasov@immelman.ru>
2019-12-27 02:26:14 +0300

    Install plugin for eager loading pivot models

--- app/Author.php
+++ app/Author.php
@@ -3,8 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use AjCastro\EagerLoadPivotRelations\EagerLoadPivotTrait;
 
 class Author extends Model
 {
-    //
+    use EagerLoadPivotTrait;
+
+
+    protected $fillable = ['first_name', 'last_name'];
 }
